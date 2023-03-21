5092f840cabb8fab7fb5272f2bc4417345f8d58c
RaulCruzOrtega <raul_cruz_ortega@outlook.es>
2023-03-21 10:34:14 +0000

    Dynamic Load Reviews

---
+++ javaScript_Dynamic/DynamicLoad_Reviews.js
@@ -0,0 +1,52 @@
+addEventListener('load', init);
+
+    const supportsTemplate = function () {
+        //create a template element and make sure it has a 'content' property
+        return 'content' in document.createElement('template');
+    }
+
+    function init() {
+
+        loadTemplate('templates/review.html', 'template_reseña_añadir', cargacontenido);
+    }
+
+    function loadTemplate(fileName, id, callback) {
+
+        fetch(fileName).then((res) => {
+            return res.text();
+        }).then((text) => {
+            document.getElementById(id).innerHTML = text;
+            //console.log(text)
+
+            if(callback){
+                callback();
+            }
+        })
+    }
+
+    function cargacontenido(){
+        if (supportsTemplate()) {
+            fetch('json/reviews.json')
+            .then(response => response.json())
+            .then(data => {
+                const contenidos = data.valoraciones;
+                let contenidoPrincipal = document.querySelector('#reseñas');
+                console.log(contenidoPrincipal);
+                let temp = document.getElementById('reseña');
+                const fragment = document.createDocumentFragment();
+                let content = temp.content;
+                console.log(content);
+                contenidos.forEach(contenido => {
+                    let aux = content.cloneNode(true);
+                    aux.querySelector('details').querySelector('.reseña').textContent = contenido.título;
+                    let estrella = "★";
+                    let valoracion = estrella.repeat(contenido.valoración);
+                    aux.querySelector('details').querySelector('.estrellas').textContent = valoracion;                
+                    aux.querySelector('details').querySelector('.text').textContent = contenido.reseña;
+                    console.log(aux);
+                    fragment.appendChild(aux);
+                });
+                contenidoPrincipal.appendChild(fragment);
+            });
+        }
+    }
