143edd0087769bedfb987bc2e7c7110f5924da5f
lauragonzs <lauragonzsuarez@gmail.com>
2023-03-21 23:26:00 +0000

    Dynamic Load Treatment Cards added

--- javaScript_Dynamic/DynamicLoad_Treatment.js
+++ javaScript_Dynamic/DynamicLoad_Treatment.js
@@ -1,33 +1,33 @@
 addEventListener('load', init);
 
-    const supportsTemplate = function () {
-        //create a template element and make sure it has a 'content' property
-        return 'content' in document.createElement('template');
-    }
+const supportsTemplate = function () {
+    //create a template element and make sure it has a 'content' property
+    return 'content' in document.createElement('template');
+}
 
-    function init() {
+function init() {
+    loadTemplate('templates/distribucion3.html', 'distribu3', cargacontenido);
+    loadTemplate('templates/card.html', 'cardsTreatment', cargaAlergenos);
+    loadTemplate('templates/card.html', 'cardsTreatment', cargaProductosUsados);
+    loadTemplate('templates/card.html', 'cardsTreatment', cargaSesiones);
+}
 
-        loadTemplate('templates/distribucion3.html', 'distribu3', cargacontenido);
-        loadTemplate('templates/card.html', 'cardsTreatment', cargacartas);
-    }
+function loadTemplate(fileName, id, callback) {
+    fetch(fileName).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById(id).innerHTML = text;
+        //console.log(text)
 
-    function loadTemplate(fileName, id, callback) {
+        if (callback) {
+            callback();
+        }
+    })
+}
 
-        fetch(fileName).then((res) => {
-            return res.text();
-        }).then((text) => {
-            document.getElementById(id).innerHTML = text;
-            //console.log(text)
-
-            if(callback){
-                callback();
-            }
-        })
-    }
-
-    function cargacontenido(){
-        if (supportsTemplate()) {
-            fetch('json/treatments.json')
+function cargacontenido() {
+    if (supportsTemplate()) {
+        fetch('json/treatments.json')
             .then(response => response.json())
             .then(data => {
                 const contenidos = data.tratamientos;
@@ -40,20 +40,102 @@ addEventListener('load', init);
                 let tratamiento = sessionStorage.getItem("TratamientoSeleccionado");
                 contenidos.forEach(contenido => {
                     if (tratamiento === contenido.nombre) {
-                    let aux = content.cloneNode(true);
-                    aux.querySelector('figure').querySelector('img').setAttribute("src", contenido.imagen);
-                    aux.querySelector('section').querySelector('.introText').textContent = contenido.descripcion;
-                    document.querySelector('.title').querySelector('.titulo').textContent = tratamiento;
-                    console.log(aux);
-                    fragment.appendChild(aux);
-                    sessionStorage.removeItem("TratamientoSeleccionado");
+                        let aux = content.cloneNode(true);
+                        aux.querySelector('figure').querySelector('img').setAttribute("src", contenido.imagen);
+                        aux.querySelector('section').querySelector('.introText').textContent = contenido.descripcion;
+                        document.querySelector('.title').querySelector('.titulo').textContent = tratamiento;
+                        console.log(aux);
+                        fragment.appendChild(aux);
                     }
                 });
                 contenidoPrincipal.appendChild(fragment);
             });
-        }
     }
+}
 
-    function cargacartas(){
-        
+function cargaAlergenos() {
+    if (supportsTemplate()) {
+        fetch('json/treatments.json')
+            .then(response => response.json())
+            .then(data => {
+                const contenidos = data.tratamientos;
+                let contenidoPrincipal = document.querySelector('#cardsTreatment');
+                console.log(contenidoPrincipal);
+                let temp = document.getElementById('card_template');
+                const fragment = document.createDocumentFragment();
+                let content = temp.content;
+                console.log(content);
+                let tratamiento = sessionStorage.getItem("TratamientoSeleccionado");
+                contenidos.forEach(contenido => {
+                    if (tratamiento === contenido.nombre) {
+                        let aux = content.cloneNode(true);
+                        aux.querySelector('#img_card').setAttribute("src", contenido.imagen);
+                        let seccionconte = aux.querySelector('section');
+                        seccionconte.querySelector('h3').textContent = "Alérgenos";
+                        seccionconte.querySelector('p').textContent = contenido.alergenos;
+                        console.log(aux);
+                        fragment.appendChild(aux);
+                    }
+                });
+                contenidoPrincipal.appendChild(fragment);
+            });
     }
+}
+
+function cargaProductosUsados() {
+    if (supportsTemplate()) {
+        fetch('json/treatments.json')
+            .then(response => response.json())
+            .then(data => {
+                const contenidos = data.tratamientos;
+                let contenidoPrincipal = document.querySelector('#cardsTreatment');
+                console.log(contenidoPrincipal);
+                let temp = document.getElementById('card_template');
+                const fragment = document.createDocumentFragment();
+                let content = temp.content;
+                console.log(content);
+                let tratamiento = sessionStorage.getItem("TratamientoSeleccionado");
+                contenidos.forEach(contenido => {
+                    if (tratamiento === contenido.nombre) {
+                        let aux = content.cloneNode(true);
+                        aux.querySelector('#img_card').setAttribute("src", contenido.imagen);
+                        let seccionconte = aux.querySelector('section');
+                        seccionconte.querySelector('h3').textContent = "Productos Usados";
+                        seccionconte.querySelector('p').textContent = contenido.productos_usados;
+                        console.log(aux);
+                        fragment.appendChild(aux);
+                    }
+                });
+                contenidoPrincipal.appendChild(fragment);
+            });
+    }
+}
+
+function cargaSesiones() {
+    if (supportsTemplate()) {
+        fetch('json/treatments.json')
+            .then(response => response.json())
+            .then(data => {
+                const contenidos = data.tratamientos;
+                let contenidoPrincipal = document.querySelector('#cardsTreatment');
+                console.log(contenidoPrincipal);
+                let temp = document.getElementById('card_template');
+                const fragment = document.createDocumentFragment();
+                let content = temp.content;
+                console.log(content);
+                let tratamiento = sessionStorage.getItem("TratamientoSeleccionado");
+                contenidos.forEach(contenido => {
+                    if (tratamiento === contenido.nombre) {
+                        let aux = content.cloneNode(true);
+                        aux.querySelector('#img_card').setAttribute("src", contenido.imagen);
+                        let seccionconte = aux.querySelector('section');
+                        seccionconte.querySelector('h3').textContent = "Sesiones";
+                        seccionconte.querySelector('p').textContent = contenido.sesiones;
+                        console.log(aux);
+                        fragment.appendChild(aux);
+                    }
+                });
+                contenidoPrincipal.appendChild(fragment);
+            });
+    }
+}
