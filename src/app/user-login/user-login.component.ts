41fb61629bf066f9f0b85f4a136f8b65e01d0e3e
lauragonzs <lauragonzsuarez@gmail.com>
2023-05-02 23:21:35 +0100

    user-login finished and fetchEmail() method added

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,70 +1,89 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { DatabaseService } from '../services/database.service';
 import { User } from '../interfaces/user.interface';
 import { Router } from '@angular/router';
 import { AutencaciónUserServiceService } from '../services/autencacion-user-service.service';
 
-
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent {
-
   loginForm!: FormGroup;
   users!: User[];
   aparecenErrores = false;
   exist: boolean = true;
-  userError: string = "Este usuario no está registrado."
+  userError: string = '';
 
-  constructor(private builder: FormBuilder, private database: DatabaseService, private router: Router, private userAuth: AutencaciónUserServiceService) { }
+  constructor(
+    private builder: FormBuilder,
+    private database: DatabaseService,
+    private router: Router,
+    private userAuth: AutencaciónUserServiceService
+  ) { }
 
   ngOnInit(): void {
-    this.loginForm = this.initForm()
+    this.loginForm = this.initForm();
 
-    this.database.getUsers()
-      .subscribe(users => { this.users = users })
+    this.database.getUsers().subscribe((users) => {
+      this.users = users;
+    });
   }
 
   initForm(): FormGroup {
     return this.builder.group({
-      userEmail: ['', [
-        Validators.required,
-        Validators.pattern(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,4}$/)
-      ]],
-      password: ['', [
-        Validators.required
-      ]]
-    })
+      userEmail: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,4}$/),
+        ],
+      ],
+      password: ['', [Validators.required]],
+    });
   }
 
-  userValidation() {
-    this.exist = this.users.some(user => user.userEmail === this.loginForm.value.userEmail);
+  async userValidation() {
+    await this.userAuth
+      .fetchEmail(this.loginForm.value.userEmail)
+      .then((res) => {
+        if (res.length === 0) {
+          this.exist = false;
+          this.userError = 'Este usuario no está registrado.';
+        }
+      });
   }
 
   login(email: string, password: string) {
-    let res = this.userAuth.login(email, password)
-      .then(res => {
+    this.userAuth.login(email, password).then(
+      (res) => {
         this.router.navigate(['/main']);
-      }, err => {
-        this.userError = "Contraseña incorrecta."
-        this.exist = false
-      })
+      },
+      (err) => {
+        this.userError = 'Contraseña incorrecta.';
+        this.exist = false;
+      }
+    );
   }
 
-  onSubmit() {
-    this.aparecenErrores = true
+  async onSubmit() {
+    this.exist = true;
+    this.aparecenErrores = true;
     if (this.loginForm.valid) {
-      this.userValidation();
+      await this.userValidation();
       if (this.exist) {
-        this.login(this.loginForm.value.userEmail, this.loginForm.value.password)
-      }
-      else {
-        this.userError = "Este usuario no está registrado."
+        this.login(
+          this.loginForm.value.userEmail,
+          this.loginForm.value.password
+        );
       }
     }
   }
-
 }
